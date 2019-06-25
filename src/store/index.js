c327b9567b3d9e52a27fcf9ff44cb9fff8dfe40a
KIyo Nishimura <kiyonish@gmail.com>
2019-06-25 20:59:14 +1000

    First commit

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/RootReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function configureStore() {
+  return createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunk),
+  ));
+}
